4b465d8725d73f009355b38e7baaa1595912b1dc
satinder singh <satindersingh472@gmail.com>
2023-05-12 15:03:36 +0530

    added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div>
-<p>This is the vue js practice deploy by satinder singh, I am adding the continous deployment.</p>
+<p>This is the vue js practice deploy by satinder singh, I am adding the continous deployment.It was amazing experience</p>
   </div>
 </template>
 
